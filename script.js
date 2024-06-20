e4639f1ec097a08be8fbcbeefca15a4a7ab9c69a
benfive01 <benfive01@gmail.com>
2024-06-20 22:12:54 +0100

    Logic behind the game i.e who wins(computer or player)
    scissors cuts paper or rock breaks scissors or paper engulfs rock. if same options are selected, it's a tie

--- script.js
+++ script.js
@@ -18,3 +18,14 @@ function getPlayerChoice(){
 
 }
  
+// //check if player's choice is same with computer as this is a tie. Check with other options too
+
+function checkTheChoices(playerChoice, computerChoice){
+    if(playerChoice === computerChoice){
+    return ("it's a tie");
+    }else if((playerChoice === 'rock' && computerChoice === 'scissors') || (playerChoice === 'scissors' && computerChoice === 'paper') || (playerChoice === 'paper' && computerChoice === 'rock')){
+    return ('You WIN');
+    }else{
+    return ('Computer WIN');
+    }
+    }
